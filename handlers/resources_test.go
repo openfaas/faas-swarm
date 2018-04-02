ae2aacf583a4e4d8d295c521bd7956ed4f6aa4f7
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-04-02 09:04:31 +0100

    Increase coverage of resource parsing
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- handlers/resources_test.go
+++ handlers/resources_test.go
@@ -4,6 +4,7 @@
 package handlers
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/openfaas/faas/gateway/requests"
@@ -28,6 +29,38 @@ func megaBytes(mbs int64) int64 {
 	return 1024 * 1024 * mbs
 }
 
+func Test_ParseMemoryLimits(t *testing.T) {
+	want := int64(256)
+	req := requests.CreateFunctionRequest{
+		Requests: &requests.FunctionResources{},
+		Limits: &requests.FunctionResources{
+			Memory: fmt.Sprintf("%d m", want),
+		},
+	}
+
+	res := buildResources(&req)
+
+	if res.Limits.MemoryBytes != megaBytes(want) {
+		t.Fatalf("Limits.MemoryBytes want: %d, got: %d", megaBytes(want), res.Limits.MemoryBytes)
+	}
+}
+
+func Test_ParseMemoryRequests(t *testing.T) {
+	want := int64(256)
+	req := requests.CreateFunctionRequest{
+		Requests: &requests.FunctionResources{
+			Memory: fmt.Sprintf("%d m", want),
+		},
+		Limits: &requests.FunctionResources{},
+	}
+
+	res := buildResources(&req)
+
+	if res.Reservations.MemoryBytes != megaBytes(want) {
+		t.Fatalf("Reservations.MemoryBytes want: %d, got: %d", megaBytes(want), res.Reservations.MemoryBytes)
+	}
+}
+
 func Test_ParseInvalidMemoryInMegabytes(t *testing.T) {
 	req := requests.CreateFunctionRequest{
 		Requests: &requests.FunctionResources{
@@ -43,37 +76,7 @@ func Test_ParseInvalidMemoryInMegabytes(t *testing.T) {
 	}
 }
 
-func TestBuildSwarmResourcesAddsCPULimits(t *testing.T) {
-	req := requests.CreateFunctionRequest{
-		Requests: &requests.FunctionResources{},
-		Limits: &requests.FunctionResources{
-			CPU: "100",
-		},
-	}
-
-	res := buildResources(&req)
-
-	if res.Limits.NanoCPUs != 100 {
-		t.Fatalf("Expected CPU limit of 100, got %d", res.Limits.NanoCPUs)
-	}
-}
-
-func TestBuildSwarmResourcesAddsCPUReservations(t *testing.T) {
-	req := requests.CreateFunctionRequest{
-		Requests: &requests.FunctionResources{
-			CPU: "100",
-		},
-		Limits: &requests.FunctionResources{},
-	}
-
-	res := buildResources(&req)
-
-	if res.Reservations.NanoCPUs != 100 {
-		t.Fatalf("Expected CPU limit of 100, got %d", res.Reservations.NanoCPUs)
-	}
-}
-
-func TestBuildSwarmResourcesWithInvalidCPUSetsReservationsTo0(t *testing.T) {
+func TestInvalidMemoryRequests_Ignored(t *testing.T) {
 	req := requests.CreateFunctionRequest{
 		Requests: &requests.FunctionResources{
 			Memory: "invalid",
@@ -84,6 +87,84 @@ func TestBuildSwarmResourcesWithInvalidCPUSetsReservationsTo0(t *testing.T) {
 	res := buildResources(&req)
 
 	if res.Reservations != nil {
-		t.Fatalf("Expected cpu reservation to be nil due to invalid inputs")
+		t.Fatalf("Expected reservations to be nil due to invalid input")
+	}
+}
+
+func TestInvalidMemoryLimits_Ignored(t *testing.T) {
+	req := requests.CreateFunctionRequest{
+		Limits: &requests.FunctionResources{
+			Memory: "invalid",
+		},
+		Requests: &requests.FunctionResources{},
+	}
+
+	res := buildResources(&req)
+
+	if res.Limits != nil {
+		t.Fatalf("Expected limits to be nil due to invalid input")
+	}
+}
+
+func TestBuildSwarmResourcesAddsCPULimits(t *testing.T) {
+	want := int64(1000000)
+
+	req := requests.CreateFunctionRequest{
+		Requests: &requests.FunctionResources{},
+		Limits: &requests.FunctionResources{
+			CPU: fmt.Sprintf("%d", want),
+		},
+	}
+
+	res := buildResources(&req)
+
+	if res.Limits.NanoCPUs != want {
+		t.Fatalf("Expected CPU limit of %d, got %d", want, res.Limits.NanoCPUs)
+	}
+}
+
+func TestBuildSwarmResourcesAddsCPUReservations(t *testing.T) {
+	want := int64(1000000)
+	req := requests.CreateFunctionRequest{
+		Requests: &requests.FunctionResources{
+			CPU: fmt.Sprintf("%d", want),
+		},
+		Limits: &requests.FunctionResources{},
+	}
+
+	res := buildResources(&req)
+
+	if res.Reservations.NanoCPUs != want {
+		t.Fatalf("Expected CPU limit of %d, got %d", want, res.Reservations.NanoCPUs)
+	}
+}
+
+func TestInvalidCPULimits_Ignored(t *testing.T) {
+	req := requests.CreateFunctionRequest{
+		Requests: &requests.FunctionResources{},
+		Limits: &requests.FunctionResources{
+			CPU: "invalid",
+		},
+	}
+
+	res := buildResources(&req)
+
+	if res.Limits != nil {
+		t.Fatalf("Expected Limits to be nil due to invalid input")
+	}
+}
+
+func TestInvalidCPURequests_Ignored(t *testing.T) {
+	req := requests.CreateFunctionRequest{
+		Limits: &requests.FunctionResources{},
+		Requests: &requests.FunctionResources{
+			CPU: "invalid",
+		},
+	}
+
+	res := buildResources(&req)
+
+	if res.Reservations != nil {
+		t.Fatalf("Expected Requests to be nil due to invalid input")
 	}
 }
