f2fd3155cd1178ead6ad290610221ddcf5fc5688
Lucas Roesler <roesler.lucas@gmail.com>
2018-07-23 09:15:47 +0200

    Enable dep prune for unused packages
    **What**
    - Enable dep's prune setting so that we only vendor the required
    packages and sub-packages
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- vendor/github.com/openfaas/faas/gateway/handlers/service_query.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-// ServiceQuery provides interface for replica querying/setting
-type ServiceQuery interface {
-	GetReplicas(service string) (currentReplicas uint64, maxReplicas uint64, minReplicas uint64, err error)
-	SetReplicas(service string, count uint64) error
-}
